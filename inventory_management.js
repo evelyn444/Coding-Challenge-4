a8c15223ded7a05fc5bdf1d62138f71464229e43
Evelyn_ Hernandez <eh22@usf.edu>
2024-09-15 19:08:31 -0400

    Initialize inventory with product objects

---
+++ inventory_management.js
@@ -0,0 +1,9 @@
+// Task 1: Create an Inventory Array of Product Objects
+const inventory = [
+    { name: 'Lipstick', price: 15, quantity: 50, lowStockLevel: 5 },
+    { name: 'Foundation', price: 20, quantity: 70, lowStockLevel: 7 },
+    { name: 'Blush', price: 12, quantity: 30, lowStockLevel: 3 },
+    { name: 'Eyeshadow', price: 10, quantity: 40, lowStockLevel: 4 },
+    { name: 'Concealer', price: 25, quantity: 60, lowStockLevel: 6},
+
+];
